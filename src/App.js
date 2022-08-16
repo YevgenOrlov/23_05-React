ecab12f5ebf4a280c96f50e76fc46f632d7c9c33
YevgenOrlov <101797140+YevgenOrlov@users.noreply.github.com>
2022-08-16 20:12:48 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import "./App.css/App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hello React</h1>
+      <p className="text">Hello React text</p>
+      <select>
+        <option value="1">One</option>
+        <option value="2">One</option>
+      </select>
+      <hr />
+      <img
+        src="/images/7423887_react_react native_icon.png "
+        alt="react"
+        title="react"
+      />
+      <div>react div</div>
+    </>
+  );
+}
+
+export default App;
